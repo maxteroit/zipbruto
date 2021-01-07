eabf5041eeba0237ab632a2a277e4f4c0ae3043f
Leo Feradero Nugraha <leoferaderonugraha@gmail.com>
2021-01-07 11:53:47 +0700

    adding some error detection

--- zipbruto.py
+++ zipbruto.py
@@ -21,20 +21,35 @@ def get_info():
     brute(filename,wordlist)
 
 def brute(filename,wordlist):
-    with open(wordlist,"r") as word:
-        for line in tqdm(word):
-            try:
-                password = line.strip()
-                with zipfile.ZipFile(filename) as file:
-                    file.setpassword(password.encode('ascii'))
-                    file.extractall()
-                    print("==================================================")
-                    print(f'Password found !! The password is >>>> {password}')
-                    print("==================================================")
-                    print("Success Extract Zip File !!")
-                    print("==================================================")
+    isFound = False
+    try:
+        with open(wordlist,"r") as word:
+            for line in tqdm(word):
+                try:
+                    password = line.strip()
+                    with zipfile.ZipFile(filename) as file:
+                        file.setpassword(password.encode('ascii'))
+                        file.extractall()
+                        print("==================================================")
+                        print(f'Password found !! The password is >>>> [{password}]')
+                        print("========== Success Extract Zip File !! ===========")
+                        print("==================================================")
+                        isFound = True
+                        break
+                except NotImplementedError:
+                    print("\nFile compression method is not supported yet.")
                     break
-            except:
-                pass
+                except zipfile.BadZipFile:
+                    print("\nBad zip file.")
+                    break
+                except RuntimeError:
+                    pass
+    except Exception as e:
+        print(f"An error occured.\n{e}")
+    if isFound == False:
+        print("==================================================")
+        print("=============== Password Not Found ===============")
+        print("======= Failed to Extract the Zip File !! ========")
+        print("==================================================")
     print("Finished..!")
 get_info()
